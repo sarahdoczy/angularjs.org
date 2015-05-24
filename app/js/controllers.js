958baa1b4cdeb2ca11dd25c44201366006cc1013
Sarah Doczy <Sarah.Doczy@sephora.com>
2015-05-24 16:52:00 -0700

    section 6 of tutorial

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -2,17 +2,36 @@
 
 /* Controllers */
 
+
 var phonecatApp = angular.module('phonecatApp', []);
+
+phonecatApp.controller('PhoneListCtrl', ['$scope', '$http',
+  function ($scope, $http) {
+    $http.get('phones/phones.json').success(function(data) {
+      $scope.phones = data;
+    });
+
+    $scope.orderProp = 'age';
+  }]);
+
+
+/*
 
 phonecatApp.controller('PhoneListCtrl', function($scope) {
   $scope.phones = [
     {'name': 'Nexus S',
-     'snippet': 'Fast just got faster with Nexus S.'},
+     'snippet': 'Fast just got faster with Nexus S.',
+     'age': 1},
     {'name': 'Motorola XOOM™ with Wi-Fi',
-     'snippet': 'The Next, Next Generation tablet.'},
+     'snippet': 'The Next, Next Generation tablet.',
+     'age': 2},
     {'name': 'MOTOROLA XOOM™',
-     'snippet': 'The Next, Next Generation tablet.'}
+     'snippet': 'The Next, Next Generation tablet.',
+     'age': 3}
   ];
+
+  $scope.orderProp = "age";
 
   $scope.name = "World";
 });
+*/
